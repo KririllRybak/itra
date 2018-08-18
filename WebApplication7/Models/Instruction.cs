dfe1489e31884dc9280b13dc283a88e9f4cd5adc
KririllRybak <dzabolotski99@gmail.com>
2018-08-18 13:47:14 +0300

    Добавьте файлы проекта.

---
+++ WebApplication7/Models/Instruction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using WebApplication7.Models;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication7.Models
+{
+    public class Instruction
+    {
+        [Key]
+        public int InstUserId { get; set; }
+
+        public string Insturctionname { get; set; }
+
+        public string Description { get; set; }
+
+        public virtual ICollection<User> GetUsers{ get; set; }
+    }
+}
